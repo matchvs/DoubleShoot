2175d0d4c8d36c782ec8ef2c6e13b4857b54bd82
Mac <570768995@qq.com>
2018-08-15 16:22:17 +0800

    fix

--- assets/game/script/uiGamePanel.js
+++ assets/game/script/uiGamePanel.js
@@ -148,6 +148,7 @@ cc.Class({
     },
 
     onDestroy() {
+        cc.audioEngine.stop(this.bgmId);
         clientEvent.off(clientEvent.eventType.roundStart, this.roundStart, this);
         clientEvent.off(clientEvent.eventType.gameOver, this.gameOver, this);
         clientEvent.off(clientEvent.eventType.refreshSlateBtn, this.refreshSlateBtn, this);
